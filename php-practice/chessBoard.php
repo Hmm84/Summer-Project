084134455481c0e3e0a5190096e745d007ef8b00
hmm84 <hamidamohamed940@gmail.com>
2025-05-27 09:04:24 -0500

    chess board mockup

---
+++ php-practice/chessBoard.php
@@ -0,0 +1,11 @@
+<?php
+
+ echo "<div>"; 
+for($x = 0; $x < 64; $x++){
+    echo "<div style=
+    'height: 50px;
+     width: 50px;
+     background-color: #000;'
+    </div>".$x;
+}
+echo "</div>"; 
