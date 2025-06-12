f24103fe21a76ee0af3cbdb2f6b3c4aaca84e5be
hmm84 <hamidamohamed940@gmail.com>
2025-06-12 11:18:41 -0500

    delete mysterious history folder?

--- .history/Include/connect_20250609142543.php
+++
@@ -1,7 +0,0 @@
-<?php
-    DEFINE('DB_HOSTNAME', 'localhost');
-    DEFINE('DB_DATABASE', 'Project');
-    DEFINE('DB_USERNAME', 'root');
-    DEFINE('DB_PASSWORD', 'root');
-    
-  
--- .history/read_story_20250609130908.php
+++
@@ -1,2 +0,0 @@
-<?php
-include("Include/init.php"); 
