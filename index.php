40e05922f3b0422d42c5e6c0983d135dba1c4571
hmm84 <hamidamohamed940@gmail.com>
2025-06-20 16:27:35 -0500

    added onto the style sheet

--- index.php
+++ index.php
@@ -1,28 +1,49 @@
 <?php
-    include("Include/init.php"); 
-    $stories = getAllStories();
- 
-    echo "
-        <!DOCTYPE html
-        <head>
-            <meta charset='utf-8'>
-            <link rel='stylesheet' href='library.css'>
-        </head>
-        
-        <body>
-        <div style='width:353px; height:776px; flex-shrink:0;'> 
-            <div class='bookshelfWrapper'>"; 
-            // foreach loop to create a list all stories
-            foreach($stories as $index => $story){
-                if ($index == 0) {
-                    echo "<div class='shelf'>";
-                }else if ($index % 2 == 0) {
-                    echo "</div> <div class='shelf'>";
-                }
-                echo "<div class='book'><a href='view_story.php?storyId=".$story['storyId']."'>".$story['title']."</a></div>";
-            }
+include("Include/init.php"); 
+$stories = getAllStories();
+$totalBooks = count($stories);
+$totalRows = 6;
+$booksPerRow = 2;
 
-    echo "
+echo "<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <link rel='stylesheet' href='library.css'>
+</head>
+<body>
+    <div class='room'>
+        <div class='wall left-wall'></div>
+        <div class='wall center-wall'>
+            <div class='bookshelf'>";
+
+// Generate rows
+for ($i = 0; $i < $totalRows; $i++) {
+    echo "<div class='book-row'>";
+    for ($j = 0; $j < $booksPerRow; $j++) {
+        $index = $i * $booksPerRow + $j;
+        if ($index < $totalBooks) {
+            $story = $stories[$index];
+            echo "<div class='book'>
+                    <a href='view_story.php?storyId=" . $story['storyId'] . "'>" . $story['title'] . "</a>
+                  </div>";
+        } else {
+            // Empty book slot
+            echo "<div class='book empty'></div>";
+        }
+    }
+    echo "</div>";
+}
+
+echo " 
+                </div>
+            </div>
+        <div class='wall right-wall'>
+        <div class='note note-left'>home</div> </div>
+        
+        <div class='floor'>
+            <div class='rug'></div>
         </div>
     </div>
-    </body>"; 
+</body>
+</html>";
