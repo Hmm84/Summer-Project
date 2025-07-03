c08cf313c6ba3d54021a7923ad9c7b87567d164e
abdisisabrahim <abdisis981@gmail.com>
2025-07-03 09:51:50 -0500

    Fixed up the posters

--- library_page.php
+++ library_page.php
@@ -9,76 +9,63 @@ echo "<!DOCTYPE html>
 <head>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <meta charset='utf-8'>
-    <link rel='stylesheet' href='library.css'>
-       
-    <style>
-     .glass{
-        background: linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
-        backdrop-filter: blur(10px);
-        -webkit-backdrop-filter: blur(10px);
-        border-radius: 20px;
-        border:1px solid rgba(255, 255, 255, 0.18);
-        box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
-    }
-    </style>
-
+    <link rel='stylesheet' href='library.css'> 
 </head>
+
 <body>
-    <div class='roomWrapper'>
+    <div class='roomWrapper'> 
         <div class='room'>
-        <div class='wall left-wall'>
-      
-            <div class='circle' style='transform: translate(-91px, -227px); order: 1;'>
-                <a href='home_page.php'>
-                <img src='fireplace.jpg'
-                style='width: 100%; height: 100%; object-fit: cover; ' />
-                </a>
-            </div>
+            <div class='wall left-wall'>
+                <div class='square' style='transform: translate(-91px, -227px); order: 1;'>
+                    <a href='home_page.php'>
+                    <img src='fireplace.jpg'
+                    style='width: 100%; height: 100%; object-fit: cover; ' />
+                    </a>
+                </div>
 
-            <div class='poster' style=' height: 251px; width: 137px; transform: translate(61px, 16px);' >
-                <img src='squirrel in poster.jpg'/>
-            </div>
+                <div class='poster' style=' height: 251px; width: 137px; transform: translate(61px, 16px);' >
+                    <img src='squirrel in poster.jpg'/>
+                </div>
 
-            <div class='poster' style='transform: translate(14px, -238px);' >
-                <img src='kabaa.jpg'  />
-            </div>
-            <div class='poster' style='transform: translate(-45px, -26px);;'>
-                <img src='kitten_pari.jpg'  />
-            </div>
-            
-
-            
-    </div>
-    <div class='wall center-wall' id='bookshelf'>
-        <div class='bookshelf' >";
-
-    for ($i = 0; $i < $totalRows; $i++) {
-        echo "<div class='book-row'>";
-        for ($j = 0; $j < $booksPerRow; $j++) {
-            $index = $i * $booksPerRow + $j;
-            if ($index < $totalBooks) {
-                $story = $stories[$index];
-                echo "<div class='book'>
-                        <a href='view_story.php?storyId=" .$story['storyId']. "'>" .$story['title']. "</a>
-                    </div>";
-            } else {
-                echo "<div class='book empty'></div>";
-            }
-        }
-        echo "</div>";
-    }
-
-    echo "</div>
+                <div class='poster' style='transform: translate(14px, -238px);' >
+                    <img src='kabaa.jpg'  />
+                </div>
+                <div class='poster' style='transform: translate(-45px, -26px);;'>
+                    <img src='kitten_pari.jpg'  />
+                </div>
         </div>
+
+        <div class='wall center-wall'>
+            <div class='bookshelf' >";
+
+                for ($i = 0; $i < $totalRows; $i++) {
+                    echo "<div class='book-row'>";
+                    for ($j = 0; $j < $booksPerRow; $j++) {
+                        $index = $i * $booksPerRow + $j;
+                        if ($index < $totalBooks) {
+                            $story = $stories[$index];
+                                    echo "<div class='book'>
+                                            <a href='view_story.php?storyId=" .$story['storyId']. "'>" .$story['title']. "</a>
+                                        </div>";
+                                } else {
+                                    echo "<div class='book empty'></div>";
+                                }
+                            }
+            echo "</div>";
+                        }
+
+            echo "</div>
+        </div>
+
         <div class='wall right-wall'>
             <div class='rectangle' style='height: 255px; width: 323px; transform: translate(62px, -239px);'>
-              <h1> HOW TO PLAY </h1> 
+            <h1> HOW TO PLAY </h1> 
             </div>
 
-            <div class='poster' style='transform: translate(-5px, 16px);'>
+            <div class='square' style='transform: translate(-5px, 16px);'>
                 <img src='kitten_heart.jpg' />
             </div>
-             <div class='circle' style='transform: translate(40px, -66px);'>
+            <div class='poster' style='transform: translate(40px, -66px);'>
                 <img src='beach.jpg' />
                 </a>
             </div>
@@ -86,10 +73,8 @@ echo "<!DOCTYPE html>
             <div class='poster' style='transform: translate(-201px, -285px);' >
                 <img src='totoro.jpg'  />
             </div>
-           
-
+            </div>
         </div>
-    </div>
         <div class='floor'>
             <div class='rug'>
             <div class='rug' style = 'width: 75%; background:rgb(232 212 177);'></div>
