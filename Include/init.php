9a26e1821cfbe46f4cca4e39670d083ca3099ead
hamida <8qf@umsystem.edu>
2025-07-30 09:43:45 -0500

    fixed config

--- Include/init.php
+++ Include/init.php
@@ -8,7 +8,7 @@ include('db_query.php');
 include('common_components.php');
 include('backend/debug_output.php'); 
 include('functions/stories.php'); 
-include('functions/config.php'); 
+include('config.php'); 
 include('functions/prompt.php'); 
 include('functions/callAI.php'); 
 include('functions/format.php'); 
