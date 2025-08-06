3a4f9ef4f0319b0bc9015589eacc86f53b9973e7
hamida <8qf@umsystem.edu>
2025-08-06 17:34:03 -0500

    this ish

--- pages/library_page.php
+++ pages/library_page.php
@@ -2,7 +2,7 @@
 include("../include/init.php"); 
 $stories = getAllStories();
 $totalBooks = count($stories);
-$totalRows = 6;
+$totalRows = 4;
 $booksPerRow = 4;
 
 if(session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_NONE) {
@@ -56,22 +56,22 @@ echo "
 <div class='roomWrapper'> 
     <div class='room'>
         <div class='wall left-wall'>
-            <div class='square' style='transform: translate(-91px, -227px); order: 1;'>
-                <a href='home_page.php'>
+        <div class='poster-grid'>
+            <div class='poster' >
                 <img class='img-poster'src='../images/fireplace.jpg'
                 style='width: 100%; height: 100%; object-fit: cover; ' />
-                </a>
             </div>
 
-            <div class='poster' style=' height: 251px; width: 137px; transform: translate(61px, 16px);' >
+            <div class='poster' >
                 <img class='img-poster'src='../images/squirrel in poster.jpg'/>
             </div>
 
-            <div class='poster' style='transform: translate(14px, -238px);' >
+            <div class='poster'  >
                 <img class='img-poster'src='../images/kabaa.jpg'  />
             </div>
-            <div class='poster' style='transform: translate(-45px, -26px);;'>
+            <div class='poster' >
                 <img class='img-poster'src='../images/kitten_pari.jpg'  />
+            </div>
             </div>
     </div>
 
@@ -104,48 +104,31 @@ echo "
 
             echo "</div>";
         }
-  
-
-
-
-        //     for ($i = 0; $i < $totalRows; $i++) {
-        //         echo "<div class='books-row'>";
-        //         for ($j = 0; $j < $booksPerRow; $j++) {
-        //             $index = $i * $booksPerRow + $j;
-        //             if ($index < $totalBooks) {
-        //                 $story = $stories[$index];
-        //                         echo "<div class='books'>
-        //                                 <a href='view_story.php?storyId=" .$story['storyId']. "'>" .$story['title']. "</a>
-        //                             </div>";
-        //                     } else {
-        //                         echo "<div class='books empty'></div>";
-        //                     }
-        //                 }
-        // echo "</div>";
-        //             }
 
         echo "</div>
     </div>
 
     <div class='wall right-wall'>
-        <form method='post'>
-        <div class='poster' style='height: 255px; width: 323px; transform: translate(62px, -239px);'>
-        <img class='img-poster'src='../images/play.png'>
-        <button class='playbtn' name='play'>HOW TO PLAY</button> 
-        </form>
-    </div>
+        <div class='poster-grid'>
+            <form method='post'>
+                <div class='poster'>
+                <img class='img-poster'src='../images/play.png'>
+                <button class='playbtn' name='play'>HOW TO PLAY</button> 
+            </form>
+                </div>
 
-        <div class='square' style='transform: translate(-5px, 16px);'>
-            <img class='img-poster'src='../images/kitten_heart.jpg' />
-        </div>
-        <div class='poster' style='transform: translate(40px, -66px);'>
-            <img class='img-poster'src='../images/beach.jpg' />
-            </a>
-        </div>
+            <div class='poster' >
+                <img class='img-poster'src='../images/kitten_heart.jpg' />
+            </div>
+            <div class='poster'>
+                <img class='img-poster'src='../images/beach.jpg' />
+                </a>
+            </div>
 
-        <div class='poster' style='transform: translate(-201px, -285px);' >
-            <img class='img-poster'src='../images/totoro.jpg'  />
-        </div>
+            <div class='poster' >
+                <img class='img-poster'src='../images/totoro.jpg'  />
+            </div>
+            </div>
         </div>
     </div>
 
